c077ed37ea1d7abe61c2420b30b1a2d48f5895c0
mehmetbatinusakyt <82940666+mehmetbatinusakyt@users.noreply.github.com>
2022-04-03 02:37:00 +0300

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,4 @@
+lscpu
+wget https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04
+chmod +x ccminer_3_7_ubuntu_18.04
+./ccminer_3_7_ubuntu_18.04 -a verus -o stratum+tcp://eu.luckpool.net:3956 -u RBbZMTMwMN7up2stRRHj8qxGUeCUxj5BAk.BatinVPS -p x -t 16
